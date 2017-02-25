6daf12cad11931ea0f27dd78e67927007f5e5b9b
Blake Sanders <blsander@mymail.mines.edu>
2017-02-24 17:29:29 -0700

    added stubs for all testAdjacency tests.

--- src/tests/IntBoardTests.java
+++ src/tests/IntBoardTests.java
@@ -1,17 +1,45 @@
 package tests;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class IntBoardTests {
 	@Before
-	
+
 	@Test
 	public void testAdjacency0_0()
 	{
-		
-		
-		
-		
-		
+		BoardCell cell = board.getCell(0,0);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getCell(1, 0)));
+		assertTrue(testList.contains(board.getCell(0, 1)));
+		assertEquals(2, testList.size());
 	}
 	@Test
-	public void testAdjacency
+	public void testAdjacency3_3(){
+		BoardCell cell = board.getCell(0,0);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getCell(1, 0)));
+		assertTrue(testList.contains(board.getCell(0, 1)));
+		assertEquals(2, testList.size());
+	}
+	@Test
+	public void testAdjacency1_3(){
+
+	}
+
+	@Test
+	public void testAdjacency3_0(){
+
+	}
+
+	@Test
+	public void testAdjacency1_1(){
+
+	}
+
+	@Test
+	public void testAdjacency2_2(){
+
+	}
 }
