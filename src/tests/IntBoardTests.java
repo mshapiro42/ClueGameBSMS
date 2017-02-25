30c4982ea5f8762ab769b0435450e570ea2178a9
Blake Sanders <blsander@mymail.mines.edu>
2017-02-24 17:41:55 -0700

    completed testAdjacency test methods.

--- src/tests/IntBoardTests.java
+++ src/tests/IntBoardTests.java
@@ -17,36 +17,55 @@ public class IntBoardTests {
 	}
 	@Test
 	public void testAdjacency3_3(){
-		BoardCell cell = board.getCell(0,0);
+		BoardCell cell = board.getCell(3,3);
 		Set<BoardCell> testList = board.getAdjList(cell);
-		assertTrue(testList.contains(board.getCell(1, 0)));
-		assertTrue(testList.contains(board.getCell(0, 1)));
+		assertTrue(testList.contains(board.getCell(3, 2)));
+		assertTrue(testList.contains(board.getCell(2, 3)));
 		assertEquals(2, testList.size());
 	}
 	@Test
 	public void testAdjacency1_3(){
-
+		BoardCell cell = board.getCell(1,3);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getCell(0, 3)));
+		assertTrue(testList.contains(board.getCell(1, 2)));
+		assertTrue(testList.contains(board.getCell(2, 3)));
+		assertEquals(3, testList.size());
 	}
 
 	@Test
 	public void testAdjacency3_0(){
-
+		BoardCell cell = board.getCell(3,0);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getCell(2, 0)));
+		assertTrue(testList.contains(board.getCell(3, 1)));
+		assertEquals(2, testList.size());
 	}
 
 	@Test
 	public void testAdjacency1_1(){
-
+		BoardCell cell = board.getCell(1,1);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getCell(0, 1)));
+		assertTrue(testList.contains(board.getCell(1, 0)));
+		assertTrue(testList.contains(board.getCell(2, 1)));
+		assertTrue(testList.contains(board.getCell(1, 2)));
+		assertEquals(4, testList.size());
 	}
 
 	@Test
 	public void testAdjacency2_2(){
-
+		BoardCell cell = board.getCell(2,2);
+		Set<BoardCell> testList = board.getAdjList(cell);
+		assertTrue(testList.contains(board.getCell(1, 2)));
+		assertTrue(testList.contains(board.getCell(2, 3)));
+		assertTrue(testList.contains(board.getCell(3, 2)));
+		assertTrue(testList.contains(board.getCell(2, 1)));
+		assertEquals(4, testList.size());
 	}
 	
 		
 	
-	@Test
-	public void testAdjacency(){}
 	
 	@Test
 	public void testTarget(){
@@ -54,6 +73,6 @@ public class IntBoardTests {
 	}
 	
 	
->>>>>>> e8f2b99d3b202f26052d42fb551edc998122b17b
+
 }
 
