d3803ac8046b980d23cc747b5897143c93f2b092
Jonny Morsicato <jmorsicato@mymail.mines.edu>
2017-03-01 22:50:52 -0700

    added test

--- src/tests/ClueGameTests.java
+++ src/tests/ClueGameTests.java
@@ -2,10 +2,12 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import clueGame.Board;
+import clueGame.BoardCell;
 
 public class ClueGameTests {
 	private static Board board;
@@ -30,10 +32,21 @@ public class ClueGameTests {
 	public void FourDoorDirections() {
 	}
 	@Test
-	public void testNumberOfDoorways(){
+	public void testNumberOfDoorways(){ // me
+		{
+			int nDoors = 0;
+			for (int i=0; i<board.getNumRows(); i++)
+				for (int j=0; j<board.getNumColumns(); j++) {
+					BoardCell cell = board.getCellAt(i, j);
+					if (cell.isDoorway())
+						nDoors++;
+				}
+			Assert.assertEquals(16, nDoors);
+		}
+	
 	}
 	@Test
-	public void testRoomInitials() {
+	public void testRoomInitials() { // me 
 	}
 	
 }
