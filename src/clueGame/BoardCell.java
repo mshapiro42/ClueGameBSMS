bec3bf0d062c532e02275f4c87291077b9d3b8af
Blake Sanders <blsander@mymail.mines.edu>
2017-03-01 22:17:41 -0700

    trying to set up BoardCell in clueGame package

---
+++ src/clueGame/BoardCell.java
@@ -0,0 +1,29 @@
+package clueGame;
+//Blake Sanders
+//Jonny Morsicato 
+public class BoardCell {
+	private int row;
+	private int col;
+	public BoardCell(int row, int col) {
+		super();
+		this.row = row;
+		this.col = col;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public void setCol(int col) {
+		this.col = col;
+	}
+	@Override
+	public String toString() {
+		return "BoardCell [row=" + row + ", col=" + col + "]";
+	}
+	public int getRow() {
+		return row;
+	}
+	public int getCol() {
+		return col;
+	}
+	
+}
