30eb1be0d9ba88d4282047cbbd4bab1c7f390219
Jonny Morsicato <jmorsicato@mymail.mines.edu>
2017-03-01 22:14:48 -0700

    Initial Set up

---
+++ src/clueGame/BoardCell.java
@@ -0,0 +1,15 @@
+package clueGame;
+
+public class BoardCell {
+
+	public boolean isDoorway() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Object[] getDoorDirection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
