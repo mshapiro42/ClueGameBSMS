1f0754868b9a9521280ecc99d4ebfa00721bbaa6
Blake Sanders <blsander@mymail.mines.edu>
2017-03-01 22:40:30 -0700

    ready to start work on tests

--- src/tests/ClueGameTests.java
+++ src/tests/ClueGameTests.java
@@ -5,15 +5,35 @@ import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class ClueGameTests {
+import clueGame.Board;
 
+public class ClueGameTests {
+	private static Board board;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		board = Board.getInstance();
+		board.setConfigFiles("ourData/clueLayout.xlsx", "ourData/Legend.txt");		
+		board.initialize();
 	}
 
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
-
+	@Test
+	public void testRooms() {
+	}
+	@Test
+	public void testBoardDimensions() {
+	}
+	@Test
+	public void FourDoorDirections() {
+	}
+	@Test
+	public void testNumberOfDoorways(){
+	}
+	@Test
+	public void testRoomInitials() {
+	}
+	
 }
