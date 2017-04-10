a19a9a811b12676e144d09c4c55b1e17f0409384
Blake Sanders <blsander@mymail.mines.edu>
2017-04-10 14:46:04 -0600

    the player cards fields are showing correctly now, with the player's hand. But the board is drawing all the players twice now

--- src/clueGame/GUI.java
+++ src/clueGame/GUI.java
@@ -20,6 +20,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.Border;
@@ -31,12 +32,13 @@ import clueGame.Card.cardType;
 
 public class GUI extends JPanel{
 	private JTextField textField;
+	private JTextArea textArea;
 	private GridBagConstraints c;
 	private static JPanel boardPanel;
 	private static JPanel playerHandPanel;
 	private static JMenuBar menuBar;
 	private Board board;	//used to get the human player's cards
-	
+	private String humanName;
 	public GUI()
 	{
 		// Create a layout with 2 rows
@@ -60,21 +62,21 @@ public class GUI extends JPanel{
 		createBoardPanel();		
 		createPlayerHandPanel();
 		//add(playerHandPanel,c);
-		
+
 
 		//Menu Bar Creation
 		menuBar = new JMenuBar();
 		menuBar.add(createFileMenu());
-	
+
 	}
-	
+
 	private JMenu createFileMenu() {
 		JMenu menu = new JMenu("File");
 		menu.add(createDetectiveItem());
 		menu.add(createFileExitItem());
 		return menu;
 	}
-	
+
 	private JMenuItem createDetectiveItem() {
 		JMenuItem item = new JMenuItem("Show Detective Notes");
 		class MenuItemListener implements ActionListener {
@@ -83,27 +85,27 @@ public class GUI extends JPanel{
 				notes.setVisible(true);
 			}
 		}
-		
+
 		item.addActionListener(new MenuItemListener());
-		
+
 		return item;
 	}
-	
+
 	private JMenuItem createFileExitItem(){
 		JMenuItem item = new JMenuItem("Exit");
-		
+
 		class MenuItemListener implements ActionListener {
 			public void actionPerformed(ActionEvent e){
 				System.exit(0);
 			}
 		}
-		
+
 		item.addActionListener(new MenuItemListener());
-		
+
 		return item;
 	}
-	
-	 private void createBoardPanel() {
+
+	private void createBoardPanel() {
 		Board board = Board.getInstance();
 		board.setConfigFiles("ClueLayout.csv", "Legend.txt", "Players.txt","Weapons.txt");
 		board.initialize();
@@ -111,91 +113,92 @@ public class GUI extends JPanel{
 		this.board = board;
 		boardPanel = board;
 	}
-	 
-	 private void createPlayerHandPanel(){
+
+	private void createPlayerHandPanel(){
 		Set<Card> hand = new HashSet<Card>();
 		for(Player p: board.getPeople()){
 			if(p.isHuman){
+				humanName = p.getName();
 				hand = p.getMyCards();
 			}
 		}
 		playerHandPanel = createPlayerCardsPanel(hand);
-	 }
+	}
 
 	private JPanel createTurnPanel() {
-		 	JPanel panel = new JPanel();
-		 	// Use a grid layout, 1 row, 2 elements (label, text)
-			panel.setLayout(new GridLayout(2,1));
-		 	JLabel label = new JLabel("Whose Turn");
-		 	label.setHorizontalAlignment(SwingConstants.CENTER);
-			textField = new JTextField();
-			textField.setColumns(40);
-			textField.setEditable(false);
-			c.gridx =0;
-			c.gridy = 6;
-			c.insets = new Insets(10,50,10,10);
-			panel.add(label);
-			panel.add(textField);
-			
-			return panel;
+		JPanel panel = new JPanel();
+		// Use a grid layout, 1 row, 2 elements (label, text)
+		panel.setLayout(new GridLayout(2,1));
+		JLabel label = new JLabel("Whose Turn");
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		textField = new JTextField();
+		textField.setColumns(40);
+		textField.setEditable(false);
+		c.gridx =0;
+		c.gridy = 6;
+		c.insets = new Insets(10,50,10,10);
+		panel.add(label);
+		panel.add(textField);
+
+		return panel;
 	}
-	 
-	 //-------------------
-	 private JPanel createDiePanel() {
-		 	JPanel panel = new JPanel();
-		 	// Use a grid layout, 1 row, 2 elements (label, text)
-			panel.setLayout(new GridLayout(1,2));
-		 	JLabel turnLabel = new JLabel("Roll");
-			textField = new JTextField();
-			textField.setColumns(15);
-			textField.setEditable(false);
-			panel.add(turnLabel);
-			panel.add(textField);
-			c.gridx = 0;
-			c.gridy = 7;
-			c.gridwidth = 1;
-			panel.setBorder(new TitledBorder (new EtchedBorder(), "Die"));
-			
-			return panel;
+
+	//-------------------
+	private JPanel createDiePanel() {
+		JPanel panel = new JPanel();
+		// Use a grid layout, 1 row, 2 elements (label, text)
+		panel.setLayout(new GridLayout(1,2));
+		JLabel turnLabel = new JLabel("Roll");
+		textField = new JTextField();
+		textField.setColumns(15);
+		textField.setEditable(false);
+		panel.add(turnLabel);
+		panel.add(textField);
+		c.gridx = 0;
+		c.gridy = 7;
+		c.gridwidth = 1;
+		panel.setBorder(new TitledBorder (new EtchedBorder(), "Die"));
+
+		return panel;
 	}
-	 
-	 private JPanel createGuessPanel() {
-		 	JPanel panel = new JPanel();
-		 	// Use a grid layout, 1 row, 2 elements (label, text)
-			panel.setLayout(new GridLayout(2,1));
-			textField = new JTextField();
-			textField.setColumns(100);
-			textField.setEditable(false);
-			panel.add(textField);
-			c.gridx=1;
-			c.gridy = 7;
-			c.gridwidth = 3;
-			panel.setBorder(new TitledBorder (new EtchedBorder(), "Guess"));
-			
-			return panel;
+
+	private JPanel createGuessPanel() {
+		JPanel panel = new JPanel();
+		// Use a grid layout, 1 row, 2 elements (label, text)
+		panel.setLayout(new GridLayout(2,1));
+		textField = new JTextField();
+		textField.setColumns(100);
+		textField.setEditable(false);
+		panel.add(textField);
+		c.gridx = 1;
+		c.gridy = 7;
+		c.gridwidth = 3;
+		panel.setBorder(new TitledBorder (new EtchedBorder(), "Guess"));
+
+		return panel;
 	}
-	 
-	 private JPanel createResultPanel() {
-		 	JPanel panel = new JPanel();
-		 	// Use a grid layout, 1 row, 2 elements (label, text)
-			panel.setLayout(new GridLayout(1,2));
-		 	JLabel turnLabel = new JLabel("Response");
-			textField = new JTextField();
-			textField.setColumns(50);
-			textField.setEditable(false);
-			panel.add(turnLabel);
-			panel.add(textField);
-			c.gridx = 4;
-			c.gridy = 7;
-			c.gridwidth =2;
-			panel.setBorder(new TitledBorder (new EtchedBorder(), "Guess result"));
-			
-			return panel;
+
+	private JPanel createResultPanel() {
+		JPanel panel = new JPanel();
+		// Use a grid layout, 1 row, 2 elements (label, text)
+		panel.setLayout(new GridLayout(1,2));
+		JLabel turnLabel = new JLabel("Response");
+		textField = new JTextField();
+		textField.setColumns(50);
+		textField.setEditable(false);
+		panel.add(turnLabel);
+		panel.add(textField);
+		c.gridx = 4;
+		c.gridy = 7;
+		c.gridwidth = 2;
+		panel.setBorder(new TitledBorder (new EtchedBorder(), "Guess result"));
+
+		return panel;
 	}
-	 
-	 
-	 //-------------------
-	 
+
+
+	//-------------------
+
 	private JPanel createButtonPanel() {
 		JPanel panel = new JPanel();
 		JButton nextPlayer = new JButton("Next player");
@@ -208,47 +211,72 @@ public class GUI extends JPanel{
 		panel.add(accusation,c);
 		return panel;
 	}
-	
+
 	public JPanel createPlayerCardsPanel(Set<Card> hand){
 		JPanel panel = new JPanel();
 		panel.setBorder(new TitledBorder (new EtchedBorder(), "My Cards"));
 		panel.setLayout(new GridLayout(5,1));
 		c.gridx = 10;
 		c.gridy = 0;
+		String text = "";
 		
 		JPanel peoplePanel = new JPanel();
 		peoplePanel.setBorder(new TitledBorder (new EtchedBorder(), "People"));
-		textField = new JTextField();
-		textField.setText("People text");
-		textField.setColumns(10);
-		textField.setEditable(false);
-		peoplePanel.add(textField);
+		textArea = new JTextArea();
+		for(Card card: hand){
+			if(card.getType() == cardType.PERSON){
+				text += card.getName() + "\n";
+			}
+		}
+		textArea.setText(text);
+		textArea.setColumns(10);
+		textArea.setEditable(false);
+		peoplePanel.add(textArea);
 		panel.add(peoplePanel, c);
-		
+		text = "";
+
 		JPanel roomPanel = new JPanel();
 		roomPanel.setBorder(new TitledBorder (new EtchedBorder(), "Rooms"));
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setEditable(false);
-		textField.setText("Rooms text");
-		roomPanel.add(textField);
+		textArea = new JTextArea();
+		for(Card card: hand){
+			if(card.getType() == cardType.ROOM){
+				text += card.getName() + "\n";
+			}
+		}
+		textArea.setColumns(10);
+		textArea.setEditable(false);
+		textArea.setText(text);
+		roomPanel.add(textArea);
 		panel.add(roomPanel, c);
+		text = "";
 
 		JPanel weaponPanel = new JPanel();
 		weaponPanel.setBorder(new TitledBorder (new EtchedBorder(), "Weapons"));
-		textField = new JTextField();
-		textField.setText("Weapons text");
-		textField.setColumns(10);
-		textField.setEditable(false);
-		weaponPanel.add(textField);
+		textArea = new JTextArea();
+		for(Card card: hand){
+			if(card.getType() == cardType.WEAPON){
+				text += card.getName() + "\n";
+			}
+		}
+		textArea.setText(text);
+		textArea.setColumns(10);
+		textArea.setEditable(false);
+		weaponPanel.add(textArea);
 		panel.add(weaponPanel, c);
-		
+		text = "";
+
 		return panel;
-		
 	}
 	
 	
-	
+
+
+
+	public String getHumanName() {
+		return humanName;
+	}
+
+
 	public static void main(String[] args) {
 		// Create a JFrame with all the normal functionality
 		JFrame frame = new JFrame();
@@ -264,7 +292,7 @@ public class GUI extends JPanel{
 		frame.setJMenuBar(menuBar);
 		frame.setVisible(true);
 		//splash field for start message, need to get the starting player's name still
-		String startMessage = "You are " + "player name" + ", press Next Player to begin player";
+		String startMessage = "You are " + gui.getHumanName() + ", press Next Player to begin player";
 		JOptionPane.showMessageDialog(frame, startMessage, "Welcome to Clue", JOptionPane.INFORMATION_MESSAGE);
 	}
 
